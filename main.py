4e4b237d10e9f4f7b1c4c0d8ded9240f8daf7e08
baloccem <emilio.balocchi@gmail.com>
2021-02-02 23:37:00 +0100

    Thermodinamics calculations added

---
+++ main.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+'''Thermodynamic module to calculate the enthalpy and entropy
+of a fluid at given temperature and pressure.
+
+TBD:    
+        - add saturated and superheated states
+        - calculation of exergy
+ 
+
+Author: Emilio Balocchi
+'''
+
+import pandas as pd
+
+class State():
+    def __init__(self,df,T,P,x,param):
+        self.T = T
+        self.P = P
+        self.x = x
+        self.df = df
+        self.param = param
+
+    def get_closest_rows(self):
+        # get closest rows in table
+        dT_sup = 1
+        dT_inf = 1
+        dP_sup = 1
+        dP_inf = 1
+        rows = self.df.loc[(self.df.index.get_level_values(level=0) > self.T-dT_inf) &
+                            (self.df.index.get_level_values(level=0) <= self.T+dT_sup)&
+                            (self.df.index.get_level_values(level=1) > self.P-dP_inf) &
+                            (self.df.index.get_level_values(level=1) <= self.P+dP_sup)]
+        return rows
+
+    def interp_K_bar(self):
+        if self.T == 'T_sat':
+           self. T = interp_T_sat(self.df,self.P,'T saturated vapour K')
+        rows = self.get_closest_rows()
+
+        # interpolate by temperature
+        slope_T = self.slope_T(rows)
+        min_value_T = self.min_value(rows,level=0)
+        max_value_T = self.max_value(rows,level=0)
+        output = self.interpolation(min_value_T,max_value_T,slope_T)
+        output = output.set_index(['T K','p [Bar]'])
+
+        # interpolation by pressure over the interpolation by temperature
+        slope_P = self.slope_P(output)
+        #print(slope_P)
+        min_value_P = self.min_value(output,level=1)
+        max_value_P = self.max_value(output,level=1)
+        output = self.interpolation(min_value_P,max_value_P,slope_P)
+
+        return (output[self.param])
+
+
+    def slope_T(self,rows):
+        # get slope to intrerpolate by temperature
+        T_pr = (self.T - rows.index.get_level_values(level=0).unique()[0]) /  (rows.index.get_level_values(level=0).unique()[1] - \
+                rows.index.get_level_values(level=0).unique()[0])
+        return T_pr
+
+    def min_value(self,rows,level):
+        min_value = rows[rows.index.get_level_values(level=level)==rows.index.get_level_values(level=level).min()]
+        min_value = min_value.reset_index()
+        return min_value
+
+    def max_value(self,rows,level):        
+        max_value = rows[rows.index.get_level_values(level=level)==rows.index.get_level_values(level=level).max()]
+        max_value = max_value.reset_index()
+        return max_value
+
+    def interpolation(self,value_min,value_max,slope):
+        output = value_min +(value_max-value_min)*slope
+        return output
+
+    def slope_P(self,rows):
+        # get slope to intrerpolate by pressure
+        P_pr = (self.P-rows.index.get_level_values(level=1)[0])/(rows.index.get_level_values(level=1)[1]- \
+        rows.index.get_level_values(level=1)[0])
+        return P_pr
+
+    def __str__(self):
+        return 'The thermodinamic property {} has a value of {}'.format(self.param, self.interp_K_bar()[0])
+
+
+#ch_exergy = pd.read_excel('ch_exergy.xls',index_col=0, header = 0, skiprows = 0 )
+tblwater = pd.read_csv('water_l.dat',' ', header = 0, skiprows = 0 )
+tblwater = tblwater.set_index(['T K','p [Bar]'])
+#tblR134a = pd.read_csv('R134a.dat',' ', header = 0, skiprows = 0 )
+#tblR134a = tblR134a.set_index(['T K','p [Bar]'])
+#tblch4 = pd.read_csv('ch4.dat',' ', header = 0, skiprows = 0 )
+#tblch4 = tblch4.set_index(['T K','p [Bar]'])
+#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
+#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
+#constPropTerm = pd.read_csv('constPropTerm.txt',' ',header = 0, skiprows = 0 )
+
+# Trying some values
+
+h1 = State(tblwater,297,1.51,0,'H saturated liquid [kJ/kg]')
+print(h1.interp_K_bar(),'\n')
+
+
+s1 = State(tblwater,297,1.51,0,'S saturated liquid [kJ/(kg K)]')
+print(s1.interp_K_bar(),'\n')
+
+
+
+
