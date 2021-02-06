2e96b754e17a5b7d0750fbd7fa0e208fa1d2b789
baloccem <emilio.balocchi@gmail.com>
2021-02-06 14:49:40 +0100

    2 tests added for subcooled fluid

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ Author: Emilio Balocchi
 '''
 
 import pandas as pd
+import unittest
+
+
+#ch_exergy = pd.read_excel('ch_exergy.xls',index_col=0, header = 0, skiprows = 0 )
+tblwater = pd.read_csv('water_l.dat',' ', header = 0, skiprows = 0 )
+tblwater = tblwater.set_index(['T K','p [Bar]'])
+#tblR134a = pd.read_csv('R134a.dat',' ', header = 0, skiprows = 0 )
+#tblR134a = tblR134a.set_index(['T K','p [Bar]'])
+#tblch4 = pd.read_csv('ch4.dat',' ', header = 0, skiprows = 0 )
+#tblch4 = tblch4.set_index(['T K','p [Bar]'])
+#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
+#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
+#constPropTerm = pd.read_csv('constPropTerm.txt',' ',header = 0, skiprows = 0 )
+
+
 
 class State():
     def __init__(self,df,T,P,x,param):
@@ -22,7 +37,12 @@ class State():
         self.param = param
 
     def get_closest_rows(self):
-        # get closest rows in table
+
+        '''
+        input: df, thermodynamic table; 
+        output: df, 4 closest rows from target T and P
+        '''
+
         dT_sup = 1
         dT_inf = 1
         dP_sup = 1
@@ -33,9 +53,14 @@ class State():
                             (self.df.index.get_level_values(level=1) <= self.P+dP_sup)]
         return rows
 
-    def interp_K_bar(self):
-        if self.T == 'T_sat':
-           self. T = interp_T_sat(self.df,self.P,'T saturated vapour K')
+        
+    def get_data_T_P(self):
+
+        '''
+        input: df, thermodynamic table; 
+        output: df, properties at defined T and P
+        '''
+
         rows = self.get_closest_rows()
 
         # interpolate by temperature
@@ -47,16 +72,30 @@ class State():
 
         # interpolation by pressure over the interpolation by temperature
         slope_P = self.slope_P(output)
-        #print(slope_P)
         min_value_P = self.min_value(output,level=1)
         max_value_P = self.max_value(output,level=1)
         output = self.interpolation(min_value_P,max_value_P,slope_P)
+        output = output.set_index(['T K','p [Bar]'])
+        return output
 
-        return (output[self.param])
+    def select_property(self):
+        return self.get_data_T_P()[self.param][0]
+
+    def check_state():
+
+        '''
+        check state against fluid saturated line
+        '''
+
+        pass
 
 
     def slope_T(self,rows):
-        # get slope to intrerpolate by temperature
+
+        '''
+        get slope to intrerpolate by temperature
+        '''
+        
         T_pr = (self.T - rows.index.get_level_values(level=0).unique()[0]) /  (rows.index.get_level_values(level=0).unique()[1] - \
                 rows.index.get_level_values(level=0).unique()[0])
         return T_pr
@@ -81,30 +120,48 @@ class State():
         rows.index.get_level_values(level=1)[0])
         return P_pr
 
+
     def __str__(self):
-        return 'The thermodinamic property {} has a value of {}'.format(self.param, self.interp_K_bar()[0])
+        return 'The thermodinamic property {} has a value of {}'.format(self.param, self.select_property())
 
-
-#ch_exergy = pd.read_excel('ch_exergy.xls',index_col=0, header = 0, skiprows = 0 )
-tblwater = pd.read_csv('water_l.dat',' ', header = 0, skiprows = 0 )
-tblwater = tblwater.set_index(['T K','p [Bar]'])
-#tblR134a = pd.read_csv('R134a.dat',' ', header = 0, skiprows = 0 )
-#tblR134a = tblR134a.set_index(['T K','p [Bar]'])
-#tblch4 = pd.read_csv('ch4.dat',' ', header = 0, skiprows = 0 )
-#tblch4 = tblch4.set_index(['T K','p [Bar]'])
-#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
-#tblmolmass = pd.read_excel('molecularmass.xls',index_col=0, header = 0, skiprows = 0 ) 
-#constPropTerm = pd.read_csv('constPropTerm.txt',' ',header = 0, skiprows = 0 )
-
-# Trying some values
+# trying some values
 
 h1 = State(tblwater,297,1.51,0,'H saturated liquid [kJ/kg]')
-print(h1.interp_K_bar(),'\n')
+h1.get_data_T_P()
+h1.select_property()
+print(h1,'\n')
 
 
 s1 = State(tblwater,297,1.51,0,'S saturated liquid [kJ/(kg K)]')
-print(s1.interp_K_bar(),'\n')
+s1.get_data_T_P()
+s1.select_property()
+print(s1,'\n')
 
 
+# ---------------------------
+#           tests
+# ---------------------------
+
+class ThermoTest(unittest.TestCase):
+    # enthalpy sub-cooled  water
+    def test_Item_1(self):
+        #self.s = State(df,T,P,x,param)
+        x = 104.8
+        self.s = State(tblwater,273.15+25,1,0,'H saturated liquid [kJ/kg]')
+        self.s.get_data_T_P()
+        self.assertAlmostEqual(self.s.select_property(), x, delta=x*0.01)
+
+    # entropy sasturated water
+    def test_Item_2(self):
+        #self.s = State(df,T,P,x,param)
+        x = 0.3673
+        self.s = State(tblwater,273.15+25,1,0,'S saturated liquid [kJ/(kg K)]')
+        self.s.get_data_T_P()
+        self.assertAlmostEqual(self.s.select_property(), x, delta=x*0.01)
+        #print('error = ', ((104.8-self.s.get_data_T_P()[0])) / \
+        #    self.s.get_data_T_P()[0] )
+
+if __name__ == "__main__":
+    unittest.main()
 
 
