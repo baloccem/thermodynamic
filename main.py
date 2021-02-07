f1ea973d890fd91b1b3150c04fa38e85414a61e3
baloccem <emilio.balocchi@gmail.com>
2021-02-07 18:54:07 +0100

    test added , main_get_properties() function

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 of a fluid at given temperature and pressure.
 
 TBD:    
-        - add saturated and superheated states
         - calculation of exergy
+        - add test for saturated conditions
+        - add more working fluids
  
 
 Author: Emilio Balocchi
@@ -16,7 +17,7 @@ import unittest
 
 
 #ch_exergy = pd.read_excel('ch_exergy.xls',index_col=0, header = 0, skiprows = 0 )
-H2O_complete = pd.read_csv('water_l.dat',' ', header = 0, skiprows = 0 )
+H2O_complete = pd.read_csv('H2O_complete.dat',' ', header = 0, skiprows = 0 )
 H2O_complete = H2O_complete.set_index(['T K','p [Bar]'])
 H2O_sat = pd.read_csv('H2O_sat.dat',' ', header = 0, skiprows = 0 )
 H2O_sat = H2O_sat.set_index(['T K','p [Bar]'])
@@ -29,7 +30,7 @@ H2O_sat = H2O_sat.set_index(['T K','p [Bar]'])
 #constPropTerm = pd.read_csv('constPropTerm.txt',' ',header = 0, skiprows = 0 )
 
 
-class State():
+class Properties():
     def __init__(self,df,df_sat,T,P,x):
         self.T = T
         self.P = P
@@ -39,32 +40,32 @@ class State():
    
     def saturation_check_state(self):
         '''
-        1.-
         check state against saturated line
         '''
-        self.P_sat = self.saturation_data().reset_index()['p [Bar]'][0]
+        if self.T <= 647.29:
+            self.P_sat = self.saturation_data().reset_index()['p [Bar]'][0]
 
-        print("The saturation pressure for the given temperature is: ", self.P_sat, ' bar')
-        if float(self.P) > float(self.P_sat):
-            print("At current P of:", self.P, " bar the state condition is subcooled liquid")
-            self.state = 0
-        elif float(self.P) < float(self.P_sat):
-            print("At current P of:", self.P, " bar the state condition is superheated")
-            self.state = 1
+            if float(self.P) > float(self.P_sat):
+                #print("At current P of:", self.P, " bar the state condition is subcooled liquid")
+                self.state = 0
+            elif float(self.P) < float(self.P_sat):
+                #print("At current P of:", self.P, " bar the state condition is superheated")
+                self.state = 1
+            else:
+                #print("At current P of:", self.P, " bar the state condition is saturated")
+                self.state = 2
         else:
-            print("At current P of:", self.P, " bar the state condition is saturated")
-            self.state = 2
+            self.state = 1
         return self.state
 
 
     def saturation_data(self):
         '''
-        1.1.-
         input: df, thermodynamic table; 
         output: df, properties at defined T and P
         '''
 
-        rows = self.saturation_closest_data()
+        rows = self.get_closest_rows('sat')
 
         # interpolate by temperature
         slope = self.slope(rows,self.T,level=0)
@@ -74,27 +75,10 @@ class State():
         output = output.set_index(['T K','p [Bar]'])
         return output
 
-    def saturation_closest_data(self):
-
-        '''
-        1.1.1.-
-        input: df, thermodynamic table; 
-        output: df, 2 closest rows from target T 
-        '''
-
-        df1 =self.df_sat.loc[self.df_sat.index.get_level_values(level=0) < self.T]
-        df1 = df1[df1.index.get_level_values(level=0).max()==df1.index.get_level_values(level=0)]
-
-        df2 =self.df_sat.loc[self.df_sat.index.get_level_values(level=0) > self.T]
-        df2 = df2[df2.index.get_level_values(level=0).min()==df2.index.get_level_values(level=0)]
-
-        return df1.append(df2)
-
-       
+           
     def get_property_data(self,rows,level):
 
         '''
-        3.-
         input: df, thermodynamic table; 
         output: df, properties at defined T and P
         '''
@@ -113,27 +97,41 @@ class State():
         return output
 
    
-    def get_closest_rows(self):
+    def get_closest_rows(self, temp_flag=''):
         '''
-        2.-
         input: df, thermodynamic table; 
         output: df, 4 closest rows from target T and P
+       
         '''
+        
+        if temp_flag == 'sat':
+            df_in = self.df_sat
+        else:
+            df_in = self.df
 
-        dT_sup = 1
-        dT_inf = 1
-        dP_sup = 1
-        dP_inf = 1
-        rows = self.df.loc[(self.df.index.get_level_values(level=0) > self.T-dT_inf) &
-                            (self.df.index.get_level_values(level=0) <= self.T+dT_sup)&
-                            (self.df.index.get_level_values(level=1) > self.P-dP_inf) &
-                            (self.df.index.get_level_values(level=1) <= self.P+dP_sup)]
-        return rows
+        df1 =df_in.loc[df_in.index.get_level_values(level=0) < self.T]
+        df1 = df1[df1.index.get_level_values(level=0).max()==df1.index.get_level_values(level=0)]
+        
+        df2 =df_in.loc[df_in.index.get_level_values(level=0) > self.T]
+        df2 = df2[df2.index.get_level_values(level=0).min()==df2.index.get_level_values(level=0)]    
+
+        self.df_out = df1.append(df2)
+        
+        if temp_flag != 'sat':
+ 
+            df3 =self.df_out.loc[self.df_out.index.get_level_values(level=1) < self.P]
+            df3 = df3[df3.index.get_level_values(level=1).max()==df3.index.get_level_values(level=1)]
+            
+            df4 =self.df_out.loc[self.df_out.index.get_level_values(level=1) > self.P]
+            df4 = df4[df4.index.get_level_values(level=1).min()==df4.index.get_level_values(level=1)] 
+
+            self.df_out = df3.append(df4)
+
+        return self.df_out
 
     def slope(self,rows,Prop,level):
 
         '''
-        3.1.1.-
         get slope to intrerpolate by defined property, level=0 temperature; level=1 pressure
         '''
         Prop_interp = (Prop - rows.index.get_level_values(level).unique()[0]) /  \
@@ -142,100 +140,122 @@ class State():
         return Prop_interp
 
     def min_value(self,rows,level):
-        '''
-        3.1.2.-
-        '''
+
         min_value = rows[rows.index.get_level_values(level=level)==rows.index.get_level_values(level=level).min()]
         min_value = min_value.reset_index()
         return min_value
 
     def max_value(self,rows,level):     
-        '''
-        3.1.3.-
-        '''  
+
         max_value = rows[rows.index.get_level_values(level=level)==rows.index.get_level_values(level=level).max()]
         max_value = max_value.reset_index()
         return max_value
 
     def interpolation(self,value_min,value_max,slope):
-        '''
-        3.1.4.-
-        '''  
+
         output = value_min +(value_max-value_min)*slope
         return output
 
-    #def __str__(self):
-    #    return ''' {} = {}'''.format(self.param, self.select_property())
-
-# trying some values
-
-# h1 = State(H2O_complete,H2O_sat,297,1.51,0,'H saturated liquid [kJ/kg]')
-# h1.select_property()
-
-# print(h1,'\n')
-
-#s = State(H2O_complete,H2O_sat,297,1.51,0,'H saturated liquid [kJ/kg]')
-#s.saturation_check_state()
-#s.main_get_properties()
+    def main_get_properties(self):
+        self.saturation_check_state()
+        get_closest_rows = self.get_closest_rows()
+        get_property_data = self.get_property_data(get_closest_rows,level=0)
+        get_property_data = self.get_property_data(get_property_data, level=1)   
+        return get_property_data
 
 
-t = State(H2O_complete,H2O_sat,297,1.51,0)
-t.saturation_check_state()
-get_closest_rows = t.get_closest_rows()
-get_property_data = t.get_property_data(get_closest_rows,level=0)
-get_data_P = t.get_property_data(get_property_data, level=1)
-print(get_data_P.T)
+    def get_property(self,liquid,gas):
+        state = self.saturation_check_state()
+        if state == 0:
+            name = liquid
+            X_total = self.main_get_properties()[name][0]   
+        elif state == 1:
+            name = gas
+            X_total = self.main_get_properties()[name][0]   
+        else:
+            X_liq = self.main_get_properties()[liquid][0]
+            X_gas = self.main_get_properties()[gas][0]
+            X_total = X_liq + self.x*(X_gas - X_liq)
+        return X_total
+
+    def get_enthalpy(self):
+        liquid = "H saturated liquid [kJ/kg]"
+        gas = "H gas [kJ/kg]"
+        return self.get_property(liquid,gas)
+
+    def get_entropy(self):
+        liquid = "S saturated liquid [kJ/(kg K)]"
+        gas = "S gas [kJ/(kg K)]"
+        return self.get_property(liquid,gas)
+
+    def __str__(self):
+        state = self.saturation_check_state()
+        A = "At current T and P the state condition is"
+        if state == 0:
+             A += " subcooled liquid"
+        elif state == 1:
+            A += " superheated"
+        else:
+            A +=" saturated"
+        
+        output = str(A) + '\n' + str(repr(self.main_get_properties().T))
+
+        return output
 
 
-#print('state: ',)
-#print(sat.saturation_data())
+# some calculations
 
-# h2 = State(H2O_complete,H2O_sat,297,1.51,0,'H saturated liquid [kJ/kg]')
-#print('***',h2.saturation_closest_data(),'\n')
+p1 = Properties(H2O_complete,H2O_sat,315.69,2.18,0)
+p1.main_get_properties()
+print(p1)
 
-# s1 = State(H2O_complete,H2O_sat,297,1.51,0,'S saturated liquid [kJ/(kg K)]')
-# s1.select_property()
-# print(s1,'\n')
+p2 = Properties(H2O_complete,H2O_sat,823,14,0)
+p2.main_get_properties()
+print(p2)
+
+# saturated condition
+p3 = Properties(H2O_complete,H2O_sat,351.15,0.4368,0.6)
+p3.main_get_properties()
+print(p2)
 
 
-#print(H2O_sat)
 
 # ---------------------------
 #           tests
 # ---------------------------
-'''
-class ThermoTest(unittest.TestCase):
-    # enthalpy sub-cooled  water
-    def test_Item_1(self):
-        #self.s = State(df,T,P,x)
-        x = 104.8
-        self.s = State(H2O_complete,H2O_sat,273.15+25,1,0,'H saturated liquid [kJ/kg]')
-        self.s.get_property_data()
-        self.s.get_data_P()
-        self.assertAlmostEqual(self.s.select_property(), x, delta=x*0.01)
 
-    # entropy sasturated water
+class ThermoTest(unittest.TestCase):
+    # enthalpy liquid water
+    def test_Item_1(self):
+        x = 104.8
+        self.p = Properties(H2O_complete,H2O_sat,273.15+25,1,0)
+        self.p.main_get_properties()
+        self.assertAlmostEqual(self.p.get_enthalpy(), x, delta=x*0.01)
+
+    # entropy liquid water
     def test_Item_2(self):
-        #self.s = State(df,T,P,x)
         x = 0.3673
-        self.s = State(H2O_complete,H2O_sat,273.15+25,1,0,'S saturated liquid [kJ/(kg K)]')
-        self.s.get_property_data()
-        self.s.get_data_P()
-        self.assertAlmostEqual(self.s.select_property(), x, delta=x*0.01)
-        #print('error = ', ((104.8-self.s.get_data_T_P()[0])) / \
-        #    self.s.get_data_T_P()[0] )
+        self.p = Properties(H2O_complete,H2O_sat,273.15+25,1,0)
+        self.p.main_get_properties()
+        self.assertAlmostEqual(self.p.get_entropy(), x, delta=x*0.01)
+
 
     # enthalpy superheated  water
     def test_Item_3(self):
-        #self.s = State(df,T,P,x)
         x = 3583.167
-        self.s = State(H2O_complete,H2O_sat,823,14,0,'H gas [kJ/kg]')
-        self.s.get_property_data()
-        self.s.get_data_P()
-        self.assertAlmostEqual(self.s.select_property(), x, delta=x*0.01)
+        self.p = Properties(H2O_complete,H2O_sat,823,14,0)
+        self.p.main_get_properties()
+        self.assertAlmostEqual(self.p.get_enthalpy(), x, delta=x*0.01)
+
+
+    # enthalpy saturated  water
+    #def test_Item_4(self):
+    #    x = 3583.167
+    #    self.p = Properties(H2O_complete,H2O_sat,823,14,0)
+    #    self.p.main_get_properties()
+    #    self.assertAlmostEqual(self.p.get_enthalpy(), x, delta=x*0.01)
 
 if __name__ == "__main__":
     unittest.main()
 
 
-'''
